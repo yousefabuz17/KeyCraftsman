5cc92514c5e31d912bd24e22c5a20ea606947666
Yousef Abuzahrieh <yousef.abuzahrieh@gmail.com>
2024-02-22 13:41:34 -0500

    v1.1.51 quick setup fix.

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
 if __name__ == "__main__":
-    setup(
-        packages=find_packages(where='src'),
-        package_dir={'': 'src'},
-        include_package_data=True,
-        package_data={'key_craftsman': ['*.json']},
-        )
+    setuptools.setup()
